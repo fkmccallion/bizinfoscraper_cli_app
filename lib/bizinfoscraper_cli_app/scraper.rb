57e8742c131cc621bef55394324d239574cd742b
Frank McCallion <fkmccallion@gmail.com>
2018-02-26 19:39:50 +0000

    update scraper class methods

--- lib/bizinfoscraper_cli_app/scraper.rb
+++ lib/bizinfoscraper_cli_app/scraper.rb
@@ -24,32 +24,33 @@ class Scraper
   end
 
   def self.additional_info(biz_hash)
-    biz_hash_array = []
+    updated_businesses = []
+
     biz_hash.each do |business_hash|
       url = BASE_PATH + business_hash[:yp_url]
       html = open(url)
       doc = Nokogiri::HTML(html)
       n = 0
+
       doc.css("article.business-card.clearfix.non-paid-listing section.primary-info div.contact p.address span").each do |biz_info|
-        while n < 4
-          if n == 0
-            business_hash[:address] = biz_info.text
-          elsif n == 1
-            business_hash[:city] = biz_info.text
-          elsif n == 2
-            business_hash[:state] = biz_info.text
-          else
-            business_hash[:zip] = biz_info.text
-          end
-          n += 1
-        end
 
-      end
-      biz_hash_array << business_hash
+        if n == 0
+          business_hash[:address] = biz_info.text
+        elsif n == 1
+          business_hash[:city] = biz_info.text
+        elsif n == 2
+          business_hash[:state] = biz_info.text
+        else
+          business_hash[:zip] = biz_info.text
+        end #end of if/else
+        n += 1
 
-    end
+      end #end of doc.css
+      updated_businesses << business_hash
 
-    biz_hash_array
-  end
+    end #end of biz_hash.each
+    updated_businesses
 
-end
+  end #end of self.additional_info
+
+end #end of scraper class
